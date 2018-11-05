ca85242a0ed6ea09a5f451f469f2309961609a26
Mohamed Abid <n.mohamedabid@gmail.com>
2018-11-05 21:41:43 +0100

    Update bayes_backprob.py

--- bayes_backprob.py
+++ bayes_backprob.py
@@ -246,29 +246,3 @@ class BBB:
         correct_prediction = tf.equal(tf.argmax(y, 1),tf.argmax(self.target,1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         return accuracy
-#%%
-        
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)        
-#%%        
-        
-conf = Config()
-
-data = tf.placeholder(tf.float32, [None, 784])
-target = tf.placeholder(tf.float32, [None, 10])
-nn = BBB(data, target, conf, is_training=True)
-
-
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
-    
-    for i in range(300000):
-        xs, ys = mnist.train.next_batch(conf.batch_size)
-        
-        _, loss_value, step, lr = sess.run(
-                [nn.optimize, nn.loss, nn.global_step, nn.learning_rate], 
-                feed_dict={nn.data: xs, nn.target: ys})
-        
-        if i % 10 == 0:
-            print("After %d training step(s),  loss on training batch is %g." % (i, loss_value))
-            print(sess.run(nn.accuracy1(), feed_dict={nn.data: xs, nn.target: ys}))                
